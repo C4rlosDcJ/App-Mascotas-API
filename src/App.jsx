c9e4f2dc13222fa79aed9a5c954eaae90ad38e0c
CarlosDc <xdcarlos888@gmail.com>
2025-02-22 20:08:05 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>🐾 Lista de Mascotas Dev🐾</h1>
+      <h1>🐾 Lista de Mascotas QA🐾</h1>
 
       {/* Buscador */}
       <div className="search-container">
